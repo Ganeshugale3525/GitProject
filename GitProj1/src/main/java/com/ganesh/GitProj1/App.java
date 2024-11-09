9c4a8a392ff5cbdb749023598c343985d44f0d4d
Ganesh Ugale <130979816+Ganeshugale3525@users.noreply.github.com>
2024-11-09 14:31:30 +0530

    Code Submitted by DEV1

--- GitProj1/src/main/java/com/ganesh/GitProj1/App.java
+++ GitProj1/src/main/java/com/ganesh/GitProj1/App.java
@@ -1,10 +1,21 @@
 package com.ganesh.GitProj1;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+
 public class App {
+	public int sum(int a,int b)
+	{
+		return a+b;
+	}
     public static void main(String[] args) {
-        System.out.println("Hello GitHub!");
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First No:");
+        int num1=sc.nextInt();
+        System.out.println("Enter Second No:");
+        int num2=sc.nextInt();
+        App app=new App();
+        int sum = app.sum(num1, num2);
+        System.out.println("Sum Is:"+sum);
+        sc.close();
     }
 }
